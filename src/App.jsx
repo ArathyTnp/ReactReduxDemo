f443d87f5474b842aa75b58a3c9becf057846914
Arathy P R <arathy.p-r@tnpconsultants.com>
2024-07-24 11:27:49 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './assets/components/header/Header'
-import ProductList from './assets/components/productList/ProductList'
+import Header from './components/header/Header'
+import ProductList from './components/productList/ProductList'
 
 function App() {
   return (
